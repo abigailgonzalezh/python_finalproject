36d4626644f0678972518e174871cae4a6a72b12
OscarCanongo <a01730443@itesm.mx>
2020-02-06 17:41:07 -0600

    Creacion de la tabla personas

---
+++ Citas.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+#Conectar a la base de datos
+conexion = sqlite3.connect("Database/Personas.sqlite3")
+
+#Seleccionar el cursor para realizar la consulta
+consulta = conexion.cursor()
+
+sql = """
+CREATE TABLE personas (
+  id INT NOT NULL,
+  nombre VARCHAR(45) NULL,
+  edad INT NULL,
+  estatura FLOAT NULL,
+  genero VARCHAR(45) NULL,
+  signo VARCHAR(45) NULL,
+  sexualidad VARCHAR(45) NULL,
+  color_ojos VARCHAR(45) NULL,
+  color_piel VARCHAR(45) NULL,
+  ingreso FLOAT NULL,
+  PRIMARY KEY (id))
+"""
+
+#Ejecutar la consulta
+if(consulta.execute(sql)): 
+    print("Tabla creada con exito")
+else:
+    print("Error al crear la tabla")
+
+#Terminando la consulta
+consulta.close()
+
+#Guardar cambios en la base de datos
+conexion.commit()
+
+#Cerrar conexion a la base de datos
