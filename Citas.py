4b12f41b9bb0aa291cc76e9ea6cd460c08827ce6
OscarCanongo <a01730443@itesm.mx>
2020-02-06 22:07:38 -0600

    Ya jalaaaaaaaaaaaa

--- Citas.py
+++ Citas.py
@@ -51,66 +51,80 @@ combo2.grid(column=1, row=6)
 #Color de ojos
 lbl6 = Label(window, text="¿Qué color de ojos deseas que tenga tu cita?")
 lbl6.grid(column=0, row=7)
-combo2 = Combobox(window)
-combo2['values']= ("Negros", "Cafe oscuro", "Cafe claro", "Verdes", "Azules",)
-combo2.current(0) #Seleccionar el valor que se mostrara
-combo2.grid(column=1, row=7)
+combo3 = Combobox(window)
+combo3['values']= ("Negros", "Cafe oscuro", "Cafe claro", "Verdes", "Azules",)
+combo3.current(0) #Seleccionar el valor que se mostrara
+combo3.grid(column=1, row=7)
 
 #Color de piel
 lbl7 = Label(window, text="¿Qué color de piel deseas que tenga tu cita?")
 lbl7.grid(column=0, row=8)
-combo3 = Combobox(window)
-combo3['values']= ("Clara", "Morena", "Blanca", "Negra")
-combo3.current(0) #Seleccionar el valor que se mostrara
-combo3.grid(column=1, row=8)
+combo4 = Combobox(window)
+combo4['values']= ("Clara", "Morena", "Blanca", "Negra")
+combo4.current(0) #Seleccionar el valor que se mostrara
+combo4.grid(column=1, row=8)
+
+edadMinima = 0
+edadMaxima = 0
+genero = ""
+signo = ""
+sexualidad = ""
+colorOjos = ""
+colorPiel = ""
 
 #Boton aceptar con funcion que guarda los datos al dar aceptar
 def clicked():
-
     res = "Bienvenido " + txt.get()
-    lbl.configure(text= res)
- 
+    edadMinima = txtEdadMinima.get()
+    edadMaxima = txtEdadMaxima.get()
+    genero = combo.get()
+    signo = combo1.get()
+    sexualidad = combo2.get()
+    colorOjos = combo3.get()
+    colorPiel = combo4.get()
+    datos=(edadMinima,edadMaxima,genero,signo,sexualidad,colorOjos,colorPiel)
+
+    #Conectar a la base de datos 
+    conexion = sqlite3.connect("Database/Personas.sqlite3")
+
+    #Seleccionar el cursor para realizar la consulta
+    consulta = conexion.cursor()
+
+    #Codigo sql para mostrar todos los datos de la tabla
+    sql = "SELECT * FROM personas WHERE edad BETWEEN ? AND ? AND genero=? AND signo=? AND sexualidad=? AND color_ojos=? AND color_piel=?" 
+    print(datos)
+
+    #Ejecutar la consulta de consulta de datos
+    if(consulta.execute(sql,datos)): 
+        filas = consulta.fetchall()
+        if len(filas)>0:
+         for fila in filas:
+            print(fila[0], fila[1], fila[2], fila[3], fila[4],fila[5], fila[6], fila[7], fila[8], fila[9])
+         else:
+             print("Ninguna persona coincide con tu criterio de busqueda")
+    else:
+        print("Error")
+
+    #Terminando la consulta
+    consulta.close()
+
+    #Guardar cambios en la base de datos
+    conexion.commit()
+
+    #Cerrar conexion a la base de datos
+    conexion.close()
+    
 btn = Button(window, text="Aceptar", command=clicked)
-btn.grid(column=0, row=9)
- 
+btn.grid(column=0, row=9)    
+
 window.mainloop()
 
 #Preguntar al usuario datos de su posible cita
-edadMinima=int(input("Cual es la edad minima que deseas que tenga tu cita? "))
-edadMaxima=int(input("Cual es la edad maxima que deseas que tenga tu cita? "))
-genero=input("Que genero estas buscando? ")
-signo=input("Que signo estas buscando? ")
-sexualidad=input("Que sexualidad buscas? ")
-colorOjos=input("Que color de ojos buscas? ")
-colorPiel=input("Que color de piel buscas? ")
+#edadMinima=int(input("Cual es la edad minima que deseas que tenga tu cita? "))
+#edadMaxima=int(input("Cual es la edad maxima que deseas que tenga tu cita? "))
+#genero=input("Que genero estas buscando? ")
+#signo=input("Que signo estas buscando? ")
+#sexualidad=input("Que sexualidad buscas? ")
+#colorOjos=input("Que color de ojos buscas? ")
+#colorPiel=input("Que color de piel buscas? ")
 #ingreso=float(input("Que ingreso buscas? "))
-
-#Conectar a la base de datos 
-conexion = sqlite3.connect("Database/Personas.sqlite3")
-
-#Seleccionar el cursor para realizar la consulta
-consulta = conexion.cursor()
-
-#Codigo sql para mostrar todos los datos de la tabla
-sql = "SELECT * FROM personas WHERE edad BETWEEN ? AND ? AND genero=? AND signo=? AND sexualidad=? AND color_ojos=? AND color_piel=?" 
-datos = (edadMinima, edadMaxima, genero, signo, sexualidad, colorOjos, colorPiel)
-
-#Ejecutar la consulta de consulta de datos
-if(consulta.execute(sql,datos)): 
-    filas = consulta.fetchall()
-    if len(filas)>0:
-        for fila in filas:
-            print(fila[0], fila[1], fila[2], fila[3], fila[4],fila[5], fila[6], fila[7], fila[8], fila[9])
-    else:
-        print("Ninguna persona coincide con tu criterio de busqueda")
-else:
-    print("Error al agregar datos")
-
-#Terminando la consulta
-consulta.close()
-
-#Guardar cambios en la base de datos
-conexion.commit()
-
-#Cerrar conexion a la base de datos
-conexion.close()
