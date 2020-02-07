75878d4f7ab601a878119b44d2328f1bdf00d3bb
OscarCanongo <a01730443@itesm.mx>
2020-02-06 20:27:30 -0600

    Implementacion de una GUI

--- Citas.py
+++ Citas.py
@@ -1,4 +1,48 @@
 import sqlite3
+from tkinter import *
+from tkinter.ttk import *
+
+window = Tk()
+window.title("Citas app")
+window.geometry('700x400')
+
+#Pedir nombre 
+lbl = Label(window, text="¿Cuál es tu nombre?")
+lbl.grid(column=0, row=0)
+txt = Entry(window,width=10)
+txt.grid(column=1, row=0)
+
+#Pedir edad minima
+lbl1 = Label(window, text="¿Cuál es la edad minima que deseas que tenga tu cita?")
+lbl1.grid(column=0, row=2)
+txtEdadMinima = Entry(window,width=10)
+txtEdadMinima.grid(column=1, row=2)
+
+#Pedir edad Maxima
+lbl2 = Label(window, text="¿Cuál es la edad maxima que deseas que tenga tu cita?")
+lbl2.grid(column=0, row=3)
+txtEdadMaxima = Entry(window,width=10)
+txtEdadMaxima.grid(column=1, row=3)
+
+#Pedir genero
+lbl3 = Label(window, text="¿Qué genero deseas que tenga tu cita?")
+lbl3.grid(column=0, row=4)
+combo = Combobox(window)
+combo['values']= ("Masculino", "Femenino", "Otro")
+combo.current(1) #Seleccionar el valor que se mostrara
+combo.grid(column=1, row=4)
+
+#Boton aceptar con funcion que guarda los datos al dar aceptar
+def clicked():
+ 
+    res = "Bienvenido " + txt.get()
+ 
+    lbl.configure(text= res)
+ 
+btn = Button(window, text="Aceptar", command=clicked)
+btn.grid(column=0, row=5)
+ 
+window.mainloop()
 
 #Preguntar al usuario datos de su posible cita
 edadMinima=int(input("Cual es la edad minima que deseas que tenga tu cita? "))
